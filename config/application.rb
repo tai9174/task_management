84ed16046ccb8c5111ac7c90a2ebf828f0b79764
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-07-11 06:31:39 +0900

    system specとmodel spec

--- config/application.rb
+++ config/application.rb
@@ -17,12 +17,12 @@ module TaskManagement
     # the framework and any gems in your application.
     config.generators do |g|
       g.test_framework :rspec,
-                       model_specs: true,
-                       view_specs: false,
-                       helper_specs: false,
-                       routing_specs: false,
-                       controller_specs: false,
-                       request_specs: false
+                        model_specs: true,
+                        view_specs: false,
+                        helper_specs: false,
+                        routing_specs: false,
+                        controller_specs: false,
+                        request_specs: false
     end
   end
 end
--- db/migrate/20220707063602_create_tasks.rb
+++ db/migrate/20220707063602_create_tasks.rb
@@ -1,11 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.string :title, null: false
-      t.string :content, null: false
+      t.string :title
+      t.string :content
 
       t.timestamps
     end
   end
 end
-
