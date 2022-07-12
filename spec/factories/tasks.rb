606d34e7959cdf968231c4344394a7b8058c4a4c
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-07-12 15:41:09 +0900

    日時降順のテスト実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,13 +3,13 @@ FactoryBot.define do
   # 作成するテストデータの名前を「task」とします
   # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
   factory :task do        
-    title { 'taiga_title' }
-    content { 'taiga_content' }
+    title { 'test_title' }
+    content { 'test_content' }
   end
     # 作成するテストデータの名前を「second_task」とします
     # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
   factory :second_task, class: Task do
-    title { 'taiga2' }
-    content { 'content2' }
+    title { 'test_title2' }
+    content { 'test_content2' }
   end
 end
