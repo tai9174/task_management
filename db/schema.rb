c64b3a3aee302634433999c54d4f21ec9d96a018
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-07-14 15:36:37 +0900

    検索インデックスを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_14_053651) do
+ActiveRecord::Schema.define(version: 2022_07_14_062954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_07_14_053651) do
     t.datetime "expired_at"
     t.integer "status"
     t.integer "priority"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
