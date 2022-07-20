85c234767a4782d8b793e544e6c8df7d63feadc4
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-07-20 18:05:38 +0900

    ログイン・ログアウトシステムの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'tasks#index'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :tasks
-  resources :users, only: [:new,:create]
+  resources :users, only: [:new,:create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
