a85557a9e82b3c6ae18f369874c0b46483401447
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-07-23 00:34:02 +0900

    ユーザー登録のテスト実装

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   #   【共通の前準備をする】
   # end
 
-  describe '【ユーザ登録のテスト】' do
-    context '【ユーザの新規登録した場合】' do
-      it '【作成したユーザーログインできる】' do
+  describe 'ユーザ登録のテスト' do
+    context 'ユーザの新規登録した場合' do
+      it '作成したユーザーログインできる' do
         visit new_user_path
         fill_in "user[name]", with:"test_name"
         fill_in "user[email]", with:"test_email@gmail.com"
@@ -14,6 +14,12 @@ RSpec.describe 'タスク管理機能', type: :system do
         fill_in "user[password_confirmation]", with:"test_password"
         click_on "アカウントを作る"
         expect(page).to have_content 'test_nameのページ'
+      end
+    end  
+    context 'ログインせずタスク一覧画面に飛ぼうとした場合' do
+      it 'ログイン画面に遷移すること' do
+        visit tasks_path
+        expect(page).to have_content 'ログイン'
       end
     end
   end
